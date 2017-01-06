2cb78e3f55ef654f683b870115f8ec132499c559
Kyle Ellrott <kellrott@gmail.com>
2017-01-06 14:53:40 -0800

    Adding observation deck to the proxy

--- proxy.py
+++ proxy.py
@@ -55,7 +55,8 @@ if __name__ == "__main__":
     application = tornado.web.Application([
         (r"^/()$", NoCacheStaticFileHandler, dict(path=os.path.join(STATIC_DIR, "main.html"))),
         (r"^/gaia/vertex/query", ProxyHandler, dict(url="%s/gaia/vertex/query" % args.main)),
-        (r"^/gaia/vertex/find/(.*)",ProxyHandler, dict(url="%s/gaia/vertex/find/" % args.main) ),
+        (r"^/gaia/vertex/find/(.*)", ProxyHandler, dict(url="%s/gaia/vertex/find/" % args.main) ),
+        (r"^/observation-deck/(.*)", ProxyHandler, dict(url="%s/observation-deck/" % args.main) ),
         (r"^/static/(.*)", NoCacheStaticFileHandler, dict(path=STATIC_DIR) )
     ])
 
